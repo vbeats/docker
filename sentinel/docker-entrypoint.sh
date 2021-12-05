9225a84ce0699acd0eef640251d35f33c86459f2
bootvue <bootvue@gmail.com>
2021-12-05 12:58:14 +0800

    Update docker-entrypoint.sh

--- sentinel/docker-entrypoint.sh
+++ sentinel/docker-entrypoint.sh
@@ -2,7 +2,10 @@
 
 set -e
 
-java -Dserver.port=8080 -Dcsp.sentinel.dashboard.server=localhost:8080 \
+java -Dserver.port=8080 \
+     -Dcsp.sentinel.metric.file.total.count=6 \
+     -Dcsp.sentinel.log.output.type=console \
+     -Dcsp.sentinel.dashboard.server=localhost:8080 \
      -Dproject.name=sentinel-dashboard -Dsentinel.dashboard.auth.username=$SENTINEL_USERNAME \
      -Dsentinel.dashboard.auth.password=$SENTINEL_PASSWORD -jar sentinel.jar \
      > /dev/stdout 2>&1
